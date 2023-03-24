b6e3c421384c7318503711611f7c2abad7d92138
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-24 16:23:37 +0500

    Initial commit

---
+++ src/test/kotlin/com/example/petclinicdocuments/PetClinicDocumentsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.petclinicdocuments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetClinicDocumentsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
